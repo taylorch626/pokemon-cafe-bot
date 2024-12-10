878129c0ba2c18e3a1e9920a72d090af60b6c290
Taylor Hansen <35126466+taylorch626@users.noreply.github.com>
2024-12-10 14:11:37 -0800

    add shell script for cron jobs

---
+++ pcafe_cancellations_checker.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+/home/taylorhansen/.pyenv/versions/pcafe/bin/python /home/taylorhansen/scripts/pcafe_cancellations.py --num_of_guests=4 --max_attempts=70 --desired_dates 10-4 10-5 10-6 10-7 10-8
